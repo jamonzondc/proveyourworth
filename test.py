42556a8bf152b6042e2ec6a8c0220bc019f94504
Jorge Alberto Monzón del Campo <jamonzondc@gmail.com>
2020-05-19 14:02:43 -0400

    otra via

--- test.py
+++ test.py
@@ -44,16 +44,17 @@ def step3_getPaylodAndSign():
 
 
 def step4_send():
-    
-    data = {
-        "email": "jamonzondc@gmail.com",
-        "name": "Jorge Alberto Monzón del Campo",
-        "aboutme": "Apasionado a la tecnología y siempre queriendo aprender más. Solo espero de ser aceptado poder estar a la altura de lo esperado… y para ello pondré lo mejor de mí con el objetivo de contribuir modestamente, con el desarrollo de vuestra empresa.",
-        "code":"https://github.com/jamonzondc/proveyourworth.git",
+    file = {
+        "code": "https://github.com/jamonzondc/proveyourworth.git",
         "resume": "https://github.com/jamonzondc/proveyourworth/blob/master/resume.pdf",
         "image": "https://github.com/jamonzondc/proveyourworth/blob/master/image.jpeg"
     }
-    request = session.post(urlToSendInfo, data=data)
+    data = {
+        "email": "jamonzondc@gmail.com",
+        "name": "Jorge Alberto Monzón del Campo",
+        "aboutme": "Apasionado a la tecnología y siempre queriendo aprender más. Solo espero de ser aceptado poder estar a la altura de lo esperado… y para ello pondré lo mejor de mí con el objetivo de contribuir modestamente, con el desarrollo de vuestra empresa."
+    }
+    request = session.post(urlToSendInfo, data=data, files=file)
     print(request.status_code)
     print(request.text)
 
