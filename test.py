6837681d81305fefcd0c00516d1e85a83b312cb2
Jorge Alberto Monzón del Campo <jamonzondc@gmail.com>
2020-05-19 13:57:28 -0400

    otra via

--- test.py
+++ test.py
@@ -48,7 +48,7 @@ def step4_send():
     data = {
         "email": "jamonzondc@gmail.com",
         "name": "Jorge Alberto Monzón del Campo",
-        "aboutme": "Apasionado a la tecnología y siempre queriendo aprender más. Solo espero de ser aceptado poder estar a la altura de lo esperado… y para ello pondré lo mejor de mí con el objetivo de contribuir modestamente, con el desarrollo de vuestra empresa."
+        "aboutme": "Apasionado a la tecnología y siempre queriendo aprender más. Solo espero de ser aceptado poder estar a la altura de lo esperado… y para ello pondré lo mejor de mí con el objetivo de contribuir modestamente, con el desarrollo de vuestra empresa.",
         "code":"https://github.com/jamonzondc/proveyourworth.git",
         "resume": "https://github.com/jamonzondc/proveyourworth/blob/master/resume.pdf",
         "image": "https://github.com/jamonzondc/proveyourworth/blob/master/image.jpeg"
